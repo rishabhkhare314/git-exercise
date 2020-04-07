2725199fae395af0de095607eae6547cbe38f502
rishabhkhare314 <31706963+rishabhkhare314@users.noreply.github.com>
2020-04-08 00:11:35 +0530

    Update App.js

--- watch exercise/src/App.js
+++ watch exercise/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Clock from './component/Clock'
+import Clock1 from './component/Clock1';
 function App() {
   return (
     <div className="App">
       <Clock />
+      <Clock1 />
     </div>
   );
 }
